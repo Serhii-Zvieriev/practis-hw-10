21ab3e9f3fe5a90ac82fa9fda55cc7f04b100074
Serhii-Zvieriev <zacjok@gmail.com>
2021-07-25 18:51:30 +0300

    complite

---
+++ src/js/fn.js
@@ -0,0 +1,33 @@
+import createMenu from '../templates/menu.hbs';
+import menu from '../data/menu.json';
+
+(function () {
+  const listRef = document.querySelector('.js-menu');
+  const switchRef = document.querySelector('#theme-switch-toggle');
+  const bodyRef = document.querySelector('body');
+
+  const { LIGHT, DARK } = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+  let state = JSON.parse(localStorage.getItem('theme'));
+
+  bodyRef.classList.add(state?.theme ? state.theme : LIGHT);
+  switchRef.checked = state?.checked;
+
+  listRef.insertAdjacentHTML('beforeend', createMenu(menu));
+
+  const changeTheme = ({ target: { checked } }) => checked ? toggleTheme(DARK, LIGHT) : toggleTheme(LIGHT, DARK);
+
+  switchRef.addEventListener('change', changeTheme);
+
+  function toggleTheme(add, rem) {
+    bodyRef.classList.replace(rem, add);
+    const state = {
+      theme: add,
+      checked: add === DARK,
+    };
+    localStorage.setItem('theme', JSON.stringify(state));
+  }
+})();
